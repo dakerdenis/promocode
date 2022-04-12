5f87401cdc5ccb1b4b799a72d493771fcee0b6f1
Denis Akershteyn <61094355+dakerdenis@users.noreply.github.com>
2022-04-12 08:43:42 +0400

    Add files via upload

---
+++ second.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="shortcut icon" type="image/png" href="../icons/logotype.png">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap">
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <!--Start Header-->
+            <header class="header">
+                <img src="../icons/logotype.png">
+            </header>
+            <!--Start Main-->
+            <main class="main">
+                <div class="banner_one">
+                    <img src="../icons/image_one.png">
+                </div>
+                <div class="post_one">
+                    <img src="../icons/Meditasiya2.png">
+                </div>
+                <div class="movies">
+                    <img src="../icons/Plashka2.png">
+                </div>
+            </main>
+            <!--Start Footer-->
+            <footer class="footer">
+                <img src="../icons/Baba.png">
+            </footer>
+        </div>
+    </body>
+</html>
